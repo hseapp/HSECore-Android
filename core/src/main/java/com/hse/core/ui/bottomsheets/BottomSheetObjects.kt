d4ae978a80bc5ae81c28f75b00a6285e9e3939af
top.hacker.pro.vip.luxury.man <mnemkevich@hse.ru>
2021-04-16 22:29:39 +0300

    bottom sheet fix

--- core/src/main/java/com/hse/core/ui/bottomsheets/BottomSheetObjects.kt
+++ core/src/main/java/com/hse/core/ui/bottomsheets/BottomSheetObjects.kt
@@ -32,95 +32,97 @@ object BottomSheetHolders {
     const val TYPE_SIMPLE_CHECKBOX_MULTILINE = 11
 }
 
-open class Item(val type: Int, var tag: String? = null)
+open class Item(val type: Int, var tag: String? = null) {
 
-data class SimpleTitledItem(
-    val title: String,
-    var text: String,
-    val onClick: (SimpleTitledItem, Int) -> Unit
-) : Item(TYPE_SIMPLE_TITLED_ITEM)
+    data class SimpleTitledItem(
+        val title: String,
+        var text: String,
+        val onClick: (SimpleTitledItem, Int) -> Unit
+    ) : Item(TYPE_SIMPLE_TITLED_ITEM)
 
-data class HorizontalChips(
-    val title: String
-) : Item(TYPE_HORIZONTAL_CHIPS) {
-    val adapter = HorizontalChipAdapter()
+    data class HorizontalChips(
+        val title: String
+    ) : Item(TYPE_HORIZONTAL_CHIPS) {
+        val adapter = HorizontalChipAdapter()
 
-    fun <T> setItems(list: List<T>?, predicate: (T) -> HorizontalChipItem) {
-        adapter.clear()
-        list?.forEach { adapter.items.add(predicate(it)) }
-        adapter.notifyDataSetChanged()
+        fun <T> setItems(list: List<T>?, predicate: (T) -> HorizontalChipItem) {
+            adapter.clear()
+            list?.forEach { adapter.items.add(predicate(it)) }
+            adapter.notifyDataSetChanged()
+        }
+
+        fun addItem(item: HorizontalChipItem) {
+            adapter.items.add(item)
+            adapter.notifyDataSetChanged()
+        }
+
+        fun remove(item: HorizontalChipItem) {
+            val position = adapter.items.indexOf(item)
+            adapter.items.remove(item)
+            adapter.notifyItemRemoved(position)
+        }
     }
 
-    fun addItem(item: HorizontalChipItem) {
-        adapter.items.add(item)
-        adapter.notifyDataSetChanged()
-    }
+    data class RangePicker(
+        val title: String,
+        val min: Int,
+        val max: Int,
+        val currentMin: Int,
+        val currentMax: Int,
+        val rangeText: String,
+        val listener: (RangePicker, Int, Int, Int) -> Unit
+    ) : Item(TYPE_RANGE_PICKER)
 
-    fun remove(item: HorizontalChipItem) {
-        val position = adapter.items.indexOf(item)
-        adapter.items.remove(item)
-        adapter.notifyItemRemoved(position)
-    }
+    data class Slider(
+        val title: String,
+        val max: Int,
+        val currentMax: Int,
+        val rangeText: String,
+        val listener: (Slider, Int, Int) -> Unit
+    ) : Item(TYPE_SLIDER)
+
+    data class DateFromTo(
+        val titleFrom: String,
+        val titleTo: String,
+        val currentTimeFrom: Date?,
+        val currentTimeTo: Date?,
+        val listener: (DateFromTo?, Int, Date?, Date?) -> Unit
+    ) : Item(TYPE_DATE_FROM_TO)
+
+    data class SimpleCheckbox(
+        val text: String,
+        var selected: Boolean,
+        val listener: (SimpleCheckbox, Int, Boolean) -> Unit
+    ) : Item(TYPE_SIMPLE_CHECKBOX)
+
+    data class SimpleCheckboxMultiline(
+        val text: String,
+        var selected: Boolean,
+        val listener: (SimpleCheckboxMultiline, Int, Boolean) -> Unit
+    ) : Item(TYPE_SIMPLE_CHECKBOX_MULTILINE)
+
+    data class SimpleSwitch(
+        val text: String,
+        var selected: Boolean,
+        val listener: (SimpleSwitch, Int, Boolean) -> Unit
+    ) : Item(TYPE_SIMPLE_SWITCH)
+
+    data class SimpleItem(
+        val text: String?
+    ) : Item(TYPE_SIMPLE_ITEM)
+
+    data class NumberPickerItem(
+        val prefix: String,
+        val suffix: String,
+        val initProgress: Int = 0,
+        val maxValue: Int = 100,
+        val minValue: Int = 0,
+        val stepSize: Int = 1,
+        val listener: (Int) -> Unit
+    ) : Item(TYPE_NUMBER_PICKER)
+
+    data class Header(
+        val text: String
+    ) : Item(TYPE_HEADER)
+
 }
-
-data class RangePicker(
-    val title: String,
-    val min: Int,
-    val max: Int,
-    val currentMin: Int,
-    val currentMax: Int,
-    val rangeText: String,
-    val listener: (RangePicker, Int, Int, Int) -> Unit
-) : Item(TYPE_RANGE_PICKER)
-
-data class Slider(
-    val title: String,
-    val max: Int,
-    val currentMax: Int,
-    val rangeText: String,
-    val listener: (Slider, Int, Int) -> Unit
-) : Item(TYPE_SLIDER)
-
-data class DateFromTo(
-    val titleFrom: String,
-    val titleTo: String,
-    val currentTimeFrom: Date?,
-    val currentTimeTo: Date?,
-    val listener: (DateFromTo?, Int, Date?, Date?) -> Unit
-) : Item(TYPE_DATE_FROM_TO)
-
-data class SimpleCheckbox(
-    val text: String,
-    var selected: Boolean,
-    val listener: (SimpleCheckbox, Int, Boolean) -> Unit
-) : Item(TYPE_SIMPLE_CHECKBOX)
-
-data class SimpleCheckboxMultiline(
-    val text: String,
-    var selected: Boolean,
-    val listener: (SimpleCheckboxMultiline, Int, Boolean) -> Unit
-) : Item(TYPE_SIMPLE_CHECKBOX_MULTILINE)
-
-data class SimpleSwitch(
-    val text: String,
-    var selected: Boolean,
-    val listener: (SimpleSwitch, Int, Boolean) -> Unit
-) : Item(TYPE_SIMPLE_SWITCH)
-
-data class SimpleItem(
-    val text: String?
-) : Item(TYPE_SIMPLE_ITEM)
-
-data class NumberPickerItem(
-    val prefix: String,
-    val suffix: String,
-    val initProgress: Int = 0,
-    val maxValue: Int = 100,
-    val minValue: Int = 0,
-    val stepSize: Int = 1,
-    val listener: (Int) -> Unit
-) : Item(TYPE_NUMBER_PICKER)
-
-data class Header(
-    val text: String
-) : Item(TYPE_HEADER)
